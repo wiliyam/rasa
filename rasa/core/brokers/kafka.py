fc134eb6f5e66607b6f6fe2251ccc7a5682c4ed3
wiliyam <36193746+wiliyam@users.noreply.github.com>
2022-02-28 09:08:17 +0530

    fix

--- rasa/core/brokers/kafka.py
+++ rasa/core/brokers/kafka.py
@@ -86,6 +86,7 @@ class KafkaEventBroker(EventBroker):
         self.ssl_check_hostname = ssl_check_hostname
         self.botId = kwargs.get("botId")
         self.userId = kwargs.get("userId")
+        self.botAvatar = kwargs.get('botAvatar')
 
     @classmethod
     async def from_endpoint_config(
@@ -116,6 +117,7 @@ class KafkaEventBroker(EventBroker):
                 return
         event["botId"] = self.botId
         event["userId"] = self.userId
+        event["botAvatar"] = self.botAvatar
         while retries:
             try:
                 self._publish(event)
